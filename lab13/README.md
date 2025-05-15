2f44f3592a4926b77691b3248574285393bc5d59
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2025-05-14 23:27:25 -0500

    Add lab 13

---
+++ lab13/README.md
@@ -0,0 +1,14 @@
+# Lab 13
+
+| Question                   | File                           |
+| -------------------------- | ------------------------------ |
+| Question 1                 | [question1.pdf](question1.pdf) |
+| Question 2 ((Http server)) | [index.js](index.js)           |
+| Question 3                 | [question3.pdf](question3.pdf) |
+
+### How to run the server locally:
+
+    $ git clone https://github.com/lytes20/wap-homepage.git
+    $ cd lab13
+    $ npm install
+    $ npm run dev
---
+++ lab13/index.js
@@ -0,0 +1,32 @@
+import http from "http";
+import fs from "fs";
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if ((url === "/" || url === "/home") && method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("Welcome to my website");
+    res.end();
+  } else if (url === "/image" && method === "GET") {
+    const src = fs.createReadStream("./images/anonymous.jpg");
+    res.writeHead(200, { "Content-Type": "image/jpeg" });
+    src.pipe(res);
+  } else if (url === "/pdf" && method === "GET") {
+    const stream = fs.createReadStream("./files/input.pdf");
+    res.writeHead(200, { "Content-Type": "application/pdf" });
+    stream.pipe(res);
+  } else if (url === "/about" && method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.write("About");
+    res.end();
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+});
+
+const port = "3000";
+server.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
